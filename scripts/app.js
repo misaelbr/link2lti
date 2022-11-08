db348d55f49b082838766209025a7585b7c3466f
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2022-11-08 17:01:31 -0300

    First version

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+/**
+ * @author: Misael Bandeira Silveira <misaelsileveira at unisc dot br>
+ * @version: 1.0
+ * @date: 2022-11-08
+ * @description: This script get url from input and format as a link to be used in Moodle external tool
+ */
+
+$(document).ready(function () {
+  $('form').submit(function (event) {
+    event.preventDefault();
+
+    let link = $('input[name="link"]').val();
+    console.log(link);
+    var re = new RegExp(
+      '^https:\\/\\/biblioteca-a..+.garden\\/viewer\\/([0-9]+)\\/?.*$',
+      'gm',
+    );
+    var match = re.exec(link);
+
+    console.log(match);
+    if (match !== null) {
+      let lti = `https://bibliotecaa.grupoa.com.br/lti/launch.php?bookid=${match[1]}`;
+      $('#erro').hide();
+      $('#retorno').show();
+      $('#resultado').text(lti).show();
+      navigator.clipboard.writeText(lti);
+      return;
+    } else {
+      $('#retorno').hide();
+      $('#erro').show();
+      return;
+    }
+  });
+});
